31513531c02f99b5a1a09ecff9b565a9a01ca4ad
Daniel Kats <boompigdev@gmail.com>
2013-02-21 17:08:07 -0500

    In the process of creating a mockup

--- utils.py
+++ utils.py
@@ -1,12 +1,20 @@
-def to_tkinter_coords(coords, top, offset=(0, 0)):
-	t = None
-	
-	for coord in coords:
-		t_new = (coord[0] + offset[0], top - coord[1] - offset[1])
-	
-		if t is None:
-			t = t_new
-		else:
-			t = t + t_new
-	
-	return t
+
+'''Commonly used utilities.'''
+    
+class CatanUtils():
+    @staticmethod
+    def print_dict(d):
+        for k, v in d.iteritems():
+            print "{} ==> {}".format(k, v)
+            
+    @staticmethod
+    def get_tkinter_coords(normal_person_coords):
+        '''Works on lists, too.'''
+        
+        return reduce(tuple.__add__, normal_person_coords)
+    
+    @staticmethod
+    def get_num_token_dots(num):
+        '''Return the number of dots on the token with given number'''
+        
+        return 6 - abs(7 - num)
