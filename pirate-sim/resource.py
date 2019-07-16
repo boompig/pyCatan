29e56e2dd4a67120744063f158dae4f335cb3c9a
Daniel Kats <daniel_kats@symantec.com>
2019-07-16 00:08:02 -0700

    more spacing fixes

--- pirate-sim/resource.py
+++ pirate-sim/resource.py
@@ -1,15 +1,15 @@
 class Resource(object):
 	TYPES = ["brick", "ore", "wheat", "sheep", "wood", "desert"]
-	
+
 	def __init__(self, r):
-		
+
 		if r in Resource.TYPES:
 			self.__r = r
 		else:
 			raise TypeError("Invalid type '{}'".format(r))
-			
+
 	def name(self):
 		return self.__r
-		
+
 	def __str__(self):
 		return self.name()
